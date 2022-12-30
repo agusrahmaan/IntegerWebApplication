ca4d515a31067b3f9e4edebee4d499a50978cda8
zyoler <zyoler@gmail.com>
2022-12-30 23:01:46 +0700

    Add member menu

---
+++ IntegerWebApplication/Controllers/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            var member = new List<Member>();
+
+            member.Add(new Member()
+            {
+                MemberId = 1,
+                Name = "Achmad Farhan Fauzan",
+                Division = "Education Division",
+                Position = "Secretary"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 2,
+                Name = "Achmad Fauzi Suhandi",
+                Division = "Welfare Division",
+                Position = "Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 3,
+                Name = "Agus Rahman Hidayat",
+                Division = "Spiritual Division",
+                Position = "Coordinator"
+            });
+            return View(member);
+        }
+    }
+}
---
+++ IntegerWebApplication/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace IntegerWebApplication.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Division { get; set; }
+
+        public string Position { get; set; }
+        
+    }
+}
