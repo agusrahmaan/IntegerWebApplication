b7421075a886d2e45ac0c9ce354706bb5da2f52e
zyoler <zyoler@gmail.com>
2023-01-02 11:23:24 +0700

    List and Array

---
+++ IntegerWebApplication/Controllers/CarController.cs
@@ -0,0 +1,98 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class CarController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            /*var cars = new List<Car>();
+            //IEnumerable
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 1,
+                Type = "Sedan",
+                Brand = "Tesla",
+                Varian = "Grey"
+            });
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 2,
+                Type = "Sedan",
+                Brand = "BMW",
+                Varian = "Black"
+            });
+
+            cars.Add(new Car()
+            {
+                IDRegistration = 3,
+                Type = "SUV",
+                Brand = "Tesla",
+                Varian = "Gold"
+            });*/
+
+
+            //Array
+            /*var cars = new Car[]
+            {
+                new Car {IDRegistration = 1, Type = "Sedan", Varian = "GT88 Silver", Brand = "BMW"},
+                new Car {IDRegistration = 2, Type = "SUV", Varian = "Accord", Brand = "BMW"},
+                new Car {IDRegistration = 3, Type = "Sedan", Varian = "FT76 Gold", Brand = "Tesla"},
+                new Car {IDRegistration = 4, Type = "Sedan", Varian = "RAV4", Brand = "Chevrolet"},
+                new Car {IDRegistration = 5, Type = "Jeep", Varian = "AT88", Brand = "BMW"},
+                new Car {IDRegistration = 6, Type = "SUV", Varian = "Accord", Brand = "BMW"},
+                new Car {IDRegistration = 7, Type = "Sedan", Varian = "FT76 Gold", Brand = "Tesla"},
+                new Car {IDRegistration = 8, Type = "Sedan", Varian = "RAV4", Brand = "Chevrolet"}
+            };*/
+
+            var cars = new Car[]{
+                new Car{IDRegistration = 0001, Type= "Sedan", Brand = "Toyota", Varian = "FT86"},
+                new Car{IDRegistration = 0002, Type = "SUV", Brand = "Toyota", Varian = "RAV4"},
+                new Car{IDRegistration = 0003, Type = "Sedan", Brand = "Honda", Varian = "Accord"},
+                new Car{IDRegistration = 0004, Type = "SUV", Brand = "Honda", Varian = "CRV"},
+                new Car{IDRegistration = 0005, Type = "Sedan", Brand = "Honda", Varian = "City"},
+            };
+
+
+            //ViewBag.Cars = cars;
+
+            //var search = cars.Single(x => x.IDRegistration == 2);
+            //cars.Remove(search);
+
+            //var car = cars.Where(x => x.Brand == "Tesla");
+            //ViewBag.Cars = car;
+
+            //var car = cars.Where(x => x.Type == "Sedan").FirstOrDefault();
+            //ViewBag.Cars = new List<Car>() {car};
+
+            //latihan 1
+            //var no1 = cars.Where(x => x.Brand == "Honda").FirstOrDefault();
+            //ViewBag.Cars = new List<Car>() { no1 };
+
+            //Latihan 2
+            //var no2 = cars.Where(x => x.Brand == "Honda" && x.Type == "Sedan");
+            //ViewBag.Cars = no2;
+
+            //Latihan 3
+            //var no3 = cars.Where(x => x.Brand == "Honda" && x.Varian == "City").FirstOrDefault();
+            //ViewBag.Cars = new List<Car>() { no3 };
+
+            //latihan 4
+            //var no4 = cars.Where(x => x.Brand == "Toyota");
+            //ViewBag.Cars = no4;
+
+            //Latihan 5
+            //var no5 = cars.Where(x => x.Varian == "City");
+            //ViewBag.Cars = no5;
+
+            //Latihan 6
+            var no6 = cars.Where(x => x.Type == "SUV" && x.Varian == "Accord");
+            ViewBag.Cars = no6;
+            return View();
+        }
+    }
+}
--- IntegerWebApplication/Controllers/MemberController.cs
+++ IntegerWebApplication/Controllers/MemberController.cs
@@ -32,6 +32,302 @@ namespace IntegerWebApplication.Controllers
                 Division = "Spiritual Division",
                 Position = "Coordinator"
             });
+
+            member.Add(new Member()
+            {
+                MemberId = 4,
+                Name = "Ahmad Faisal Hidayat",
+                Division = "Core Manager",
+                Position = "Chairman"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 5,
+                Name = "Ali Hanafiah",
+                Division = "Cleaning Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 6,
+                Name = "Aliya Rohaya Siregar",
+                Division = "Education Division",
+                Position = "Treasurer"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 7,
+                Name = "Amelia Siregar",
+                Division = "Core Manager",
+                Position = "Security"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 8,
+                Name = "Ananda Pratama Indra Buana",
+                Division = "Cleaning Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 9,
+                Name = "Andriansyah",
+                Division = "Cleaning Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 10,
+                Name = "Anggi Permana",
+                Division = "Core Manager",
+                Position = "Security"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 11,
+                Name = "Annur Akbar Bahagia",
+                Division = "Hostel Division",
+                Position = "Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 12,
+                Name = "Arya Kusuma Agniqoyad",
+                Division = "Spiritual Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 13,
+                Name = "Avita Diah Saputri",
+                Division = "Hostel Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 14,
+                Name = "Azhari Fahreza Lubis",
+                Division = "Welfare Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 15,
+                Name = "Bagas Rizky Herdiansyah Putra",
+                Division = "Admissions Committee",
+                Position = "Documentation"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 16,
+                Name = "Bunga Sari Hutasuhut",
+                Division = "Welfare Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 17,
+                Name = "Dede Adam Alamsyah",
+                Division = "Spiritual Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 18,
+                Name = "Dimas Firmansyah",
+                Division = "Education Division",
+                Position = "Secretary"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 19,
+                Name = "Dwi Fauzul Ahmad Tanjung",
+                Division = "Health Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 20,
+                Name = "Farhan Ramadhan Muchtar",
+                Division = "Core Manager",
+                Position = "Security"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 21,
+                Name = "Helmi Tris Edyan",
+                Division = "Cleaning Division",
+                Position = "Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 22,
+                Name = "Kharisma Amalia",
+                Division = "Core Manager",
+                Position = "Internal Treasures"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 23,
+                Name = "Krise Rohalia",
+                Division = "Spiritual Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 24,
+                Name = "Kurniawan",
+                Division = "Health Manager",
+                Position = "Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 25,
+                Name = "M Azriel Pazarudin",
+                Division = "Education Division",
+                Position = "Public Relations"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 26,
+                Name = "Maulina Inas Nasya",
+                Division = "Spiritual Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 27,
+                Name = "Maypa Dea Fazrin",
+                Division = "Hostel Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 28,
+                Name = "Melani Putri",
+                Division = "Welfare Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 29,
+                Name = "Muhammad Fadli Fathurrahman",
+                Division = "Education Division",
+                Position = "Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 30,
+                Name = "Naily Rina Pribawa",
+                Division = "Education Division",
+                Position = "Deputy Coordinator"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 31,
+                Name = "Peni Julianti",
+                Division = "Core Manager",
+                Position = "Secretary"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 32,
+                Name = "Putri Lestari",
+                Division = "Core Manager",
+                Position = "External Treasures"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 33,
+                Name = "Putri Mulyani",
+                Division = "Health Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 34,
+                Name = "Riyanda Ramadhan",
+                Division = "Hostel Manager",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 35,
+                Name = "Sawaluddin",
+                Division = "Education Division",
+                Position = "Public Relations"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 36,
+                Name = "Suci Amanah",
+                Division = "Hostel Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 37,
+                Name = "Tigana Reymansyah",
+                Division = "Edaucation Division",
+                Position = "Public Relations"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 38,
+                Name = "Toyyib Hendrasto",
+                Division = "Welfare Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 39,
+                Name = "Tri Wulandari",
+                Division = "Health Division",
+                Position = "Member"
+            });
+
+            member.Add(new Member()
+            {
+                MemberId = 40,
+                Name = "Wahyu",
+                Division = "Admissions Committee",
+                Position = "Chairman"
+            });
             return View(member);
         }
     }
